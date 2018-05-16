aecf52a2b0d7526c91da5e9b66a588f802e3cc27
farhan <pahlevi.kun@gmail.com>
2018-05-16 18:45:41 +0700

    Check Point: Define Your Data Model with Unit Test - 16/05/18 18:45

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,32 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Farhan Yuda Pahlevi on 5/16/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class Meal{
+    // MARK: Properties
+    var name:String
+    var photo:UIImage?
+    var rating:Int
+    
+    init?(name:String, photo:UIImage?, rating:Int) {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
